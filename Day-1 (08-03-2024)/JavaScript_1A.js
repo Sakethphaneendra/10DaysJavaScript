5bb6c50c1b8f5408ead49f028f62828d63bd1302
unknown <sakethphaneendra@gmail.com>
2024-03-08 23:07:11 +0530

    Day 1 Completed

---
+++ Day-1 (08-03-2024)/JavaScript_1A.js
@@ -0,0 +1,27 @@
+let username = 'Saketh'
+let user2="peetha"
+console.log(username + user2)
+
+user2='Peetha';
+console.log(username +" And " +user2)
+
+
+
+// Data Types
+/*
+1.Numbers
+2.String
+3.Boolean
+4.Null
+5.Undefined
+6.Symbol
+*/
+
+let number = 120;
+console.log(number)
+
+
+let numbser = 15.24;
+console.log(numbser)
+
+
